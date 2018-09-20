42f9942b0a59740a5675efb84db8e246fa35f5f6
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2018-09-20 17:20:30 +0300

    chore: update @turf/difference

--- test/test.js
+++ test/test.js
@@ -1,49 +1,55 @@
-var test = require('tape');
-var deintersect = require('../');
+const fs = require("fs");
+var test = require("tape");
+const difference = require("@turf/difference");
+var deintersect = require("../");
 
 var poly1 = {
-  "type": "Feature",
-  "properties": {
-    "fill": "#0f0"
+  type: "Feature",
+  properties: {
+    fill: "#0f0"
   },
-  "geometry": {
-    "type": "Polygon",
-    "coordinates": [[
-      [-46.738586, -23.596711],
-      [-46.738586, -23.458207],
-      [-46.560058, -23.458207],
-      [-46.560058, -23.596711],
-      [-46.738586, -23.596711]
-    ]]
+  geometry: {
+    type: "Polygon",
+    coordinates: [
+      [
+        [-46.738586, -23.596711],
+        [-46.738586, -23.458207],
+        [-46.560058, -23.458207],
+        [-46.560058, -23.596711],
+        [-46.738586, -23.596711]
+      ]
+    ]
   }
 };
 
 var poly2 = {
-  "type": "Feature",
-  "properties": {
-    "fill": "#00f"
+  type: "Feature",
+  properties: {
+    fill: "#00f"
   },
-  "geometry": {
-    "type": "Polygon",
-    "coordinates": [[
-      [-46.650009, -23.631314],
-      [-46.650009, -23.5237],
-      [-46.509246, -23.5237],
-      [-46.509246, -23.631314],
-      [-46.650009, -23.631314]
-    ]]
+  geometry: {
+    type: "Polygon",
+    coordinates: [
+      [
+        [-46.650009, -23.631314],
+        [-46.650009, -23.5237],
+        [-46.509246, -23.5237],
+        [-46.509246, -23.631314],
+        [-46.650009, -23.631314]
+      ]
+    ]
   }
 };
 
 var result = [
   {
-    "type": "Feature",
-    "properties": {
-      "fill": "#0f0"
+    type: "Feature",
+    properties: {
+      fill: "#0f0"
     },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
+    geometry: {
+      type: "Polygon",
+      coordinates: [
         [
           [-46.738586, -23.596711],
           [-46.738586, -23.458207],
@@ -55,29 +61,28 @@ var result = [
     }
   },
   {
-    "type": "Feature",
-    "properties": {
-      "fill": "#00f"
+    type: "Feature",
+    properties: {
+      fill: "#00f"
     },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [
+    geometry: {
+      type: "Polygon",
+      coordinates: [
         [
-          [ -46.650009, -23.631314],
-          [-46.650009, -23.596711],
-          [-46.560058, -23.596711],
-          [-46.560058, -23.5237],
-          [-46.509246, -23.5237],
+          [-46.650009, -23.631314],
           [-46.509246, -23.631314],
+          [-46.509246, -23.5237],
+          [-46.560058, -23.5237],
+          [-46.560058, -23.596711],
+          [-46.650009, -23.596711],
           [-46.650009, -23.631314]
         ]
       ]
     }
   }
-]
+];
 
-
-test('delete-intersections', function(t) {
+test("delete-intersections", function(t) {
   var differenced = deintersect([poly1, poly2]);
   t.deepEqual(differenced, result);
   t.end();
